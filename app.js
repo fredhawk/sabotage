028b0606b89c0f72a96fbff90d79d12e2febd189
osycon <fredrichawk@gmail.com>
2017-05-07 17:35:33 +0200

    Add add message check Add help message check

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,29 +6,27 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(PORT, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+  console.log(
+    'Express server listening on port %d in %s mode',
+    server.address().port,
+    app.settings.env
+  );
 });
 app.post('/', (req, res) => {
   let text = req.body.text;
-  console.log(req.body);
-  // What follows are example of response
-  if (! /^\d+$/.test(text)) {
-    res.send('U R doing it wrong. Enter a status code like 200!')
-    return;
+  // console.log(req.body);
+  const message = req.body.text.split(' ');
+  console.log(message);
+  switch (message[0]) {
+    case 'add':
+      res.json('Working!');
+    case `help`:
+      res.json(`Help please!`);
+    default:
+      res.json('Not Working!');
   }
-  let data = {
-    response_type: 'ephemeral',
-    text: '302: Found',
-    attachments: [
-      {
-        image_url: 'https://http.cat/302.jpg'
-      }
-    ]
-  }
-  // end of example
-  res.json(data)
-})
+});
