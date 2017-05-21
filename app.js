83d9037cbee9ec0a6a069e92af6504ab3a57a32c
osycon <fredrichawk@gmail.com>
2017-05-21 17:47:16 +0200

    Add a route for oauth verification

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const request = require('request');
 const mongoose = require('mongoose');
-const Resource = require('./models/resource');
 
+const Resource = require('./models/resource');
 const handleCommands = require('./commands/handleCommands');
 
 const PORT = process.env.PORT || 8080;
@@ -37,6 +37,47 @@ const server = app.listen(PORT, () => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/slack', (req, res) => {
+  if (!req.query.code) {
+    // access denied
+    res.redirect('https://sabotage-rhinos.herokuapp.com/');
+    return;
+  }
+  let data = {
+    form: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code
+    }
+  };
+  request.post('https://slack.com/api/oauth.access', data, function(
+    error,
+    response,
+    body
+  ) {
+    if (!error && response.statusCode == 200) {
+      // Get an auth token
+      let token = JSON.parse(body).access_token;
+
+      // Get the team domain name to redirect to the team URL after auth
+      request.post(
+        'https://slack.com/api/team.info',
+        { form: { token: token } },
+        function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            if (JSON.parse(body).error == 'missing_scope') {
+              res.send(`Sabotage resource bot has been added to your team!`);
+            } else {
+              let team = JSON.parse(body).team.domain;
+              res.redirect(`http://${team}.slack.com`);
+            }
+          }
+        }
+      );
+    }
+  });
+});
+
 // Route to website
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
@@ -46,9 +87,7 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   let text = req.body.text;
 
-  // console.log(req.body);
   const message = req.body.text.split(` `);
-  // console.log(message);
 
   handleCommands(message).then(data => {
     res.json(data);
